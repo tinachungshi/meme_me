5f0a9c1e54a37375b54de9874d36998312d39748
tinachungshi <tinachungshi@gmail.com>
2017-04-27 19:29:22 -0700

    Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424231607) do
+ActiveRecord::Schema.define(version: 20170428021141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170424231607) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
   add_foreign_key "memes", "users"
