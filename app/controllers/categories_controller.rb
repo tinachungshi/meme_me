130fa865d92f7bed80139399c4479fd1d6d9289f
tinachungshi <tinachungshi@gmail.com>
2017-05-02 17:06:21 -0700

    Fix category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,4 @@
 class CategoriesController < ApplicationController
 
-  # def show
-  #   @meme = Meme.find(params[:id])
-  #   @memes = @meme.all
-  # end
-
-  def show
-    @names = [{name: "fck"}, {name: "asd"}]
-
-  end
 
 end
